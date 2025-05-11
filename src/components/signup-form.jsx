9b665c91edab7007291f6cc93acb99c28728780f
sonroy-glitch <rsounak55@gmail.com>
2025-05-12 02:28:17 +0530

    loginGoogle

--- src/components/signup-form.jsx
+++ src/components/signup-form.jsx
@@ -7,9 +7,12 @@ import { Label } from "@/components/ui/label";
 import signup_video from "../media/signup_video.mp4";
 import { Checkbox } from "@/components/ui/checkbox";
 
+
 export function SignupForm({ className, ...props }) {
   const [videoReady, setVideoReady] = useState(false);
   const videoRef = useRef(null);
+  const [email,setEmail]=useState('')
+  const [password,setPassword]=useState('')
 
   useEffect(() => {
     const handleLoadedData = () => setVideoReady(true);
@@ -60,18 +63,7 @@ export function SignupForm({ className, ...props }) {
                 Sign up
               </Button>
               
-              <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
-                <span className="bg-card text-muted-foreground relative z-10 px-2">
-                  Or continue with
-                </span>
-              </div>
-
-              <Button variant="outline" type="button" className="w-full">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-5 h-5">
-                  <path d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z" fill="currentColor" />
-                </svg>
-                <span className="sr-only">Login with Google</span>
-              </Button>
+              
               
               <div className="text-center text-sm">
                 Already have an account? <a href="/login" className="underline underline-offset-4">Log in</a>
